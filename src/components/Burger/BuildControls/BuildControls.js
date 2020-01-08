9088954df1e9b83ba790c02adbddd676859769d5
Carlos Vinicius F. Gracioli <carlos.gracioli@meta.com.br>
2020-01-08 17:22:52 -0300

    adding build controls with styles

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const buildControls = (props) => (
-  <div>
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+
+]
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl, index) => (<BuildControl key={index} label={ctrl.label} />))}
   </div>
 );
 
