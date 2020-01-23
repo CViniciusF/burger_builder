ec962156dcb06d06fd8a7dcce4ab1dcd463e75ed
Carlos Vinicius F. Gracioli <carlos.gracioli@meta.com.br>
2020-01-23 11:24:28 -0300

    adding router and form validation

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,25 +2,8 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    totalPrice: 0
-  }
-
-  componentWillMount() {
-    const query = new URLSearchParams(this.props.location.search);
-    const ingredients = {};
-    let price = 0;
-    for (const param of query.entries()) {
-      if (param[0] === 'price') {
-        price = param[1];
-      } else {
-        ingredients[param[0]] = +param[1];
-      }
-    }
-    this.setState({ ingredients: ingredients, totalPrice: price })
-  }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -44,6 +27,13 @@ class Checkout extends Component {
         />
       </div>
     )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
   }
 }
 
